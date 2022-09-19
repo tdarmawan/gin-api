b58a03c190c4b3562f1613a396f5bf6a1ccd3342
tdarmawan <tdarmawan@hacktiv8.com>
2022-09-19 16:52:19 +0700

    test

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -28,7 +28,7 @@ func main() {
 	if *port != -1 {
 		portStr = fmt.Sprintf(":%d", *port)
 		listener = http.ListenAndServe
-		// http.Handle("/", http.FileServer(http.Dir("./public")))
+		http.Handle("/", http.FileServer(http.Dir("./public")))
 		http.HandleFunc("/", RenderTemplate)
 		http.HandleFunc("/login", Login)
 		http.HandleFunc("/logout", Logout)
