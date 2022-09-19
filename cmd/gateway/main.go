41915324fba28493ef281110213d84e44c47adb2
tdarmawan <tdarmawan@hacktiv8.com>
2022-09-19 16:27:08 +0700

    test

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 
 	// "github.com/carlmjohnson/feed2json"
-	"html/template"
+	// "html/template"
 
 	"github.com/carlmjohnson/gateway"
 )
@@ -29,94 +29,94 @@ func main() {
 		portStr = fmt.Sprintf(":%d", *port)
 		listener = http.ListenAndServe
 		// http.Handle("/", http.FileServer(http.Dir("./public")))
-		http.HandleFunc("/api/", RenderTemplate)
-		http.HandleFunc("/api/login", Login)
-		http.HandleFunc("/api/logout", Logout)
+		// http.HandleFunc("/api/", RenderTemplate)
+		// http.HandleFunc("/api/login", Login)
+		// http.HandleFunc("/api/logout", Logout)
 	}
 	log.Fatal(listener(portStr, nil))
 }
 
-func RenderTemplate(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		var tmpl = template.Must(template.New("form").ParseFiles("public/index.html"))
-		var err = tmpl.Execute(w, nil)
+// func RenderTemplate(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method == "GET" {
+// 		var tmpl = template.Must(template.New("form").ParseFiles("public/index.html"))
+// 		var err = tmpl.Execute(w, nil)
 
-		if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		return
-}
+// 		if err != nil {
+// 				http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		}
+// 		return
+// }
 
-http.Error(w, "", http.StatusBadRequest)
-}
+// http.Error(w, "", http.StatusBadRequest)
+// }
 
-func Login(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		var tmpl = template.Must(template.New("result").ParseFiles("public/index.html"))
+// func Login(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method == "POST" {
+// 		var tmpl = template.Must(template.New("result").ParseFiles("public/index.html"))
 
-		if err := r.ParseForm(); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-		}
-		var email = r.FormValue("email")
+// 		if err := r.ParseForm(); err != nil {
+// 				http.Error(w, err.Error(), http.StatusInternalServerError)
+// 				return
+// 		}
+// 		var email = r.FormValue("email")
 
-		emails := []string{"tdarmawan@hacktiv8.com","rendih@gmail.com","tiara@gmail.com","shelly@gmail.com","tika@gmail.com"}
-		output := GenerateBiodata(emails)
+// 		emails := []string{"tdarmawan@hacktiv8.com","rendih@gmail.com","tiara@gmail.com","shelly@gmail.com","tika@gmail.com"}
+// 		output := GenerateBiodata(emails)
 
-		var argint int
+// 		var argint int
 
-		for i, x := range emails {
-			if email == x {
-				argint = i
-			}
-		}
+// 		for i, x := range emails {
+// 			if email == x {
+// 				argint = i
+// 			}
+// 		}
 
-		for i, x := range output {
-			if argint == i {
-				var data = map[string]string{"email": email, "message":"Welcome "+ x.nama, "alamat":x.alamat, "alasan":x.alasan}
-				if err := tmpl.Execute(w, data); err != nil {
-						http.Error(w, err.Error(), http.StatusInternalServerError)
-				}
-			}
-		}
+// 		for i, x := range output {
+// 			if argint == i {
+// 				var data = map[string]string{"email": email, "message":"Welcome "+ x.nama, "alamat":x.alamat, "alasan":x.alasan}
+// 				if err := tmpl.Execute(w, data); err != nil {
+// 						http.Error(w, err.Error(), http.StatusInternalServerError)
+// 				}
+// 			}
+// 		}
 
-		return
-}
+// 		return
+// }
 
-http.Error(w, "", http.StatusBadRequest)
-}
+// http.Error(w, "", http.StatusBadRequest)
+// }
 
-func Logout(w http.ResponseWriter, r *http.Request){
-	if r.Method == "GET" {
-			var tmpl = template.Must(template.New("form").ParseFiles("login.html"))
-			var err = tmpl.Execute(w, nil)
+// func Logout(w http.ResponseWriter, r *http.Request){
+// 	if r.Method == "GET" {
+// 			var tmpl = template.Must(template.New("form").ParseFiles("login.html"))
+// 			var err = tmpl.Execute(w, nil)
 
-			if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
-			return
-	}
+// 			if err != nil {
+// 					http.Error(w, err.Error(), http.StatusInternalServerError)
+// 			}
+// 			return
+// 	}
 
-http.Error(w, "", http.StatusBadRequest)
-}
+// http.Error(w, "", http.StatusBadRequest)
+// }
 
-func GenerateBiodata(bio []string) []peserta {
-	nama := []string{"tdarmawan","rendih","Tiaramut","ShellyS","AtikaR"}
-	addr := []string{"Kota A", "Kota B", "Kota C", "Kota D", "Kota E"}
-	alasan := []string{"Alasan Thomas", "Alasan Rendi", "Alasan Tiara", "Alasan Shelly", "Alasan Tika"}
-	generate := make([]peserta, 0)
-	var p peserta
+// func GenerateBiodata(bio []string) []peserta {
+// 	nama := []string{"tdarmawan","rendih","Tiaramut","ShellyS","AtikaR"}
+// 	addr := []string{"Kota A", "Kota B", "Kota C", "Kota D", "Kota E"}
+// 	alasan := []string{"Alasan Thomas", "Alasan Rendi", "Alasan Tiara", "Alasan Shelly", "Alasan Tika"}
+// 	generate := make([]peserta, 0)
+// 	var p peserta
 
-	for key, value := range bio {
-		p.nama = nama[key]
-		p.email = value
-		p.alamat = addr[key]
-		p.alasan = alasan[key]
-		generate = append(generate, p)
-	}
+// 	for key, value := range bio {
+// 		p.nama = nama[key]
+// 		p.email = value
+// 		p.alamat = addr[key]
+// 		p.alasan = alasan[key]
+// 		generate = append(generate, p)
+// 	}
 
-	return generate
-}
+// 	return generate
+// }
 
 // func cacheControlMiddleware(h http.Handler) http.Handler {
 // 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
